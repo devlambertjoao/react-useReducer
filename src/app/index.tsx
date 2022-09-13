7930fc96e7735358ffd748f4e575aabdafbab098
Joao Lambert <joao.lambert@gft.com>
2022-09-13 16:16:28 -0300

    First

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -19,9 +19,33 @@ const App = () => {
     }, 750);
   }
 
+  const fields = [
+    {
+      id: 1,
+      title: "Name",
+      placeholder: "Your Name",
+      prop: "name"
+    },
+    {
+      id: 2,
+      title: "Email",
+      placeholder: "your_email@email.com",
+      prop: "email"
+    },
+  ]
+
   return (
     <div className="container">
       <div className="modal-container">
+        <form className="form">
+          {fields.map(field => {
+            return (
+              <label key={field.id} className="field-container">
+                <span className="field-container__title">{field.title}</span>
+                <input className="field-container__input" placeholder={field.placeholder} />
+              </label>)
+          })}
+        </form>
         <button onClick={doSomeRequestWithErrorResponse}>Do Bad request</button>
         <button onClick={doSomeRequestWithSuccessResponse}>Do Successfully request</button>
         {state.hasError && <span>Whops... Something went wrong :(</span>}
